263a761fb16493503e048f8a374fc04417332101
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2025-12-16 17:15:29 +0500

    feat: Implement ui of sidebar

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './globals.css';
+import '../app/styles/global.css';
 import { Providers } from './providers';
 
 createRoot(document.getElementById('root')!).render(
---
+++ src/widgets/sidebar/index.tsx
@@ -0,0 +1,50 @@
+import { Flex, Menu } from 'antd';
+import Sider from 'antd/es/layout/Sider';
+import { SolutionOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { useLocation, useNavigate } from 'react-router';
+import { Logo } from './logo';
+import { BottomContent } from './bottom-content';
+
+const mainLinks = [
+  {
+    key: '/',
+    icon: <SolutionOutlined />,
+    label: 'Main page',
+  },
+  {
+    key: '/ui',
+    icon: <VideoCameraOutlined />,
+    label: 'UI components',
+  },
+];
+
+export const Sidebar = ({ collapsed }: { collapsed: boolean }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <Sider
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+      theme="light"
+      width={280}
+      className="h-screen borderr">
+      <Flex className="h-full flex-col">
+        <Logo collapsed={collapsed} />
+
+        {/* TOP MENU */}
+        <Menu
+          theme="light"
+          mode="inline"
+          selectedKeys={[location.pathname]}
+          items={mainLinks}
+          onClick={({ key }) => navigate(key)}
+          className="flex-1"
+        />
+
+        <BottomContent collapsed={collapsed} />
+      </Flex>
+    </Sider>
+  );
+};
