d4b8d0548fdf0aebdb8fd834dadd7e60a356a080
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2026-01-16 18:13:07 +0500

    feat: Add PostHog analytics integration and initialize it in the main application entry point.

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../app/styles/global.css';
 import { Providers } from './providers';
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Providers />
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <Providers />
+    </PostHogProvider>
   </StrictMode>,
 );
